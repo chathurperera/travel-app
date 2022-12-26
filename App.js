99f5c6f2f20bbe958d53a68edcaafa25a28dddee
chathurperera <chathuraperera007@gmail.com>
2022-12-26 23:45:44 +0530

    Title components

--- App.js
+++ App.js
@@ -6,34 +6,13 @@ import {
   Text,
   View,
 } from 'react-native';
+import Home from './src/screens/Home';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View >
-        <Text>Hello</Text>
-      </View>
-    </SafeAreaView>
+    <Home />
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+const styles = StyleSheet.create({});
 export default App;
---
+++ src/components/Title/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+import styles from './styles';
+
+const Title = () => {
+    return (
+        <Text style={styles.title}>Title component</Text>
+    );
+
+};
+
+export default Title;
